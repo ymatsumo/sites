c440e7df1ecee50ea8a769f20d40ffb80e12e8ba
ymatsumo <yukimatsumoto0209@gmail.com>
2015-07-04 16:41:45 +0000

    Google Map表示追加(gmaps4rails)

--- smoker/app/controllers/smoking_area_controller.rb
+++ smoker/app/controllers/smoking_area_controller.rb
@@ -1,5 +1,12 @@
 class SmokingAreaController < ApplicationController
   def index
     @smoking_areas = SmokingArea.all
+
+    @hash = Gmaps4rails.build_markers(@smoking_areas) do |smoking_area, marker|
+      marker.lat smoking_area.latitude
+      marker.lng smoking_area.longitude
+      marker.infowindow smoking_area.name
+      marker.json({title: smoking_area.name})
+    end
   end
 end
