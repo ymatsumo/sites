09b8122133e68c8744a1c8dd2be48b6ab9858cc9
ymatsumo <yukimatsumoto0209@gmail.com>
2015-07-28 15:01:16 +0000

    seed追加

--- smoker/db/seeds.rb
+++ smoker/db/seeds.rb
@@ -5,5 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-SmokingArea.create(:name => '荻窪駅南口喫煙所', :address => '東京都杉並区荻窪', :latitude => 35.70428, :longitude => 139.62112)
-SmokingArea.create(:name => '荻窪駅北口喫煙所', :address => '東京都杉並区荻窪', :latitude => 35.70517, :longitude => 139.62085)
+SmokingArea.create(:name => '荻窪駅南口喫煙所', :address => '東京都杉並区荻窪', :latitude => 35.704346, :longitude => 139.621315)
+SmokingArea.create(:name => '荻窪駅北口喫煙所', :address => '東京都杉並区荻窪', :latitude => 35.705177, :longitude => 139.620860)
+SmokingArea.create(:name => 'ヒルサイド 2F テラス', :address => '東京都港区六本木６丁目 六本木ヒルズ', :latitude => 35.660659, :longitude => 139.730104)
+SmokingArea.create(:name => 'ヒルサイド 2F TOHOシネマズ 入口右側', :address => '東京都港区六本木６丁目 六本木ヒルズ', :latitude => 35.659900, :longitude => 139.729804)
